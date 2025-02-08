700308226a0bbe5c6015989d3cc2fe10e7ac7e21
quh <quhxuxm_quh@163.com>
2025-02-08 12:52:45 +0800

    initialize

--- script/install-v3.sh
+++ script/install-v3.sh
@@ -51,12 +51,10 @@ sudo cp -r /ppaass-v3/sourcecode/ppaass-v3/proxy/resources/* /ppaass-v3/build/re
 sudo cp -r /ppaass-v3/sourcecode/ppaass-v3/proxy/resources/agent_rsa/* /ppaass-v3/build/resources/agent_rsa
 sudo cp -r /ppaass-v3/sourcecode/ppaass-v3/proxy/resources/forward_rsa/* /ppaass-v3/build/resources/forward_rsa
 sudo cp /ppaass-v3/sourcecode/ppaass-v3/target/release/proxy /ppaass-v3/build/ppaass-v3-proxy
-sudo cp /ppaass-v3/sourcecode/ppaass-v3/script/start-proxy.sh /ppaass-v3/build/
-sudo cp /ppaass-v3/sourcecode/ppaass-v3/script/concrete-start-proxy.sh /ppaass-v3/build/
 sudo cp /ppaass-v3/sourcecode/ppaass-v3/target/release/tool /ppaass-v3/build/ppaass-v3-tool
+sudo cp /ppaass-v3/sourcecode/ppaass-v3/script/* /ppaass-v3/build/
 
 sudo chmod 777 /ppaass-v3/build
-sudo chmod 777 /ppaass-v3/sourcecode/ppaass-v3/script/*
 cd /ppaass-v3/build
 ls -l
 
