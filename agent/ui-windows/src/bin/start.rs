4a3afced8b82e580b38c558009a6d2ee18eb3f7c
quhxuxm_quh <quhxuxm_quh@163.com>
2025-02-23 16:45:26 +0800

    Try to add windows native ui

--- agent/ui-windows/src/bin/start.rs
+++ agent/ui-windows/src/bin/start.rs
@@ -1,3 +1,7 @@
+use nwg::NativeUi;
+use ppaass_agent_ui_windows::app::BasicApp;
 fn main() {
-    println!("Hello, world!");
+    nwg::init().expect("Failed to init Native Windows GUI");
+    let _app = BasicApp::build_ui(Default::default()).expect("Failed to build UI");
+    nwg::dispatch_thread_events();
 }
