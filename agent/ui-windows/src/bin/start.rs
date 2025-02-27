fb77a752a4284d578293bceb282ce217bce407db
quhxuxm_quh <quhxuxm_quh@163.com>
2025-02-27 17:22:27 +0800

    Try to add ui

--- agent/ui-windows/src/bin/start.rs
+++
@@ -1,7 +0,0 @@
-use nwg::NativeUi;
-use ppaass_agent_ui_windows::app::PpaassAgentApplication;
-fn main() {
-    nwg::init().expect("Failed to init Native Windows GUI");
-    let _app = PpaassAgentApplication::build_ui(Default::default()).expect("Failed to build UI");
-    nwg::dispatch_thread_events();
-}
